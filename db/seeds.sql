0b7f9efb0735b2fe82ddb182e5a765ac114129c0
“bonnielee” <bonlee8489@gmail.com>
2021-04-05 20:52:31 -0700

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,9 +7,10 @@ VALUES ('Director', 150000, 1), ('Advisor', 145000, 2),
 ('Dev', 120000, 3), ('Senior Sales', 110000, 4), ('Sales', 95000, 4),
 ('HR', 98000, 5);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Steph', 'Curry', 4, null), ('Draymond', 'Green', 5, 1), 
-('Klay', 'Thompson', 1, null), ('Kelly', 'Oubre', 5, 1),
-('Andre', 'Iguodala', 8, null), ('Steve', 'Kerr', 6, null),
-('Bob', 'Myers', 3, null), ('Shaun', 'Livingston', 2, 7),
-('Damion', 'Lee', 7, 6);
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+-- VALUES ('Steph', 'Curry', 4, null), ('Draymond', 'Green', 5, 1), 
+-- ('Klay', 'Thompson', 1, null), ('Kelly', 'Oubre', 5, 1),
+-- ('Andre', 'Iguodala', 8, null), ('Steve', 'Kerr', 6, null),
+-- ('Bob', 'Myers', 3, null), ('Shaun', 'Livingston', 2, 7),
+-- ('Damion', 'Lee', 7, 6);
+
